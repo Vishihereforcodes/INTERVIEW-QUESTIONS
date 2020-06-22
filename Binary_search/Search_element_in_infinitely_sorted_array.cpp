e8c8dbe2ec1dacd2d7ca6bfc30b8151b5ed3932e
urvashi-code1255 <urvashianand0906@gmail.com>
2020-06-22 19:10:45 +0530

    Q11

---
+++ Binary_search/Search_element_in_infinitely_sorted_array.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int *arr ,int start , int end , int n , int num)
+{
+	while(start <= end)
+	{
+		mid = start + (end - start)/2 ;
+		
+		if(arr[mid] == num) 
+			return mid ;
+		
+		else if(arr[mid] > num){
+			end = mid - 1 ;
+		}
+		
+		else
+			start = mid + 1 ;
+	}
+	return -1 ;
+}
+
+int ReducingInfiniteArray(int *arr)
+{
+    int start = 0, end = 1;
+	
+	while(num > arr[end])
+	{
+		start = end ;
+		end *= 2 ;
+	}	
+	return {start , end }; // Now these start and end values will be used to find the element.
+}
+
