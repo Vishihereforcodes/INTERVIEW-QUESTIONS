fe1efb1b46581985d95d83a4cae6ea11e1808370
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-01 15:32:50 +0530

    Q-14

--- Binary_search/Peak_Element.cpp
+++ Binary_search/Peak_Element.cpp
@@ -1,22 +1,24 @@
+// In this question,  we have to find the index of the peak element in an array which is not sorted. 
+// peak element is the element which is greater than its left and right element.
 int peakElement(int arr[], int n)
 {
   int start = 0 , end = n-1 , mid ;
   while(start<=end)
   {
       mid = start + (end - start)/2;
-      if(mid>0 && mid<n-1)
+      if(mid>0 && mid<n-1) 
       {
         if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
         return mid ;
         
-        else if(arr[mid-1]>arr[mid])
+        else if(arr[mid-1]>arr[mid]) 
         end = mid - 1;
         
         else
         start = mid + 1;
       }
       
-      else if(mid == 0)
+      else if(mid == 0) // for index 0 we have to compare it only with the element in its right
       {
           if(arr[0]>arr[1])
           return 0 ;
@@ -25,7 +27,7 @@ int peakElement(int arr[], int n)
           return 1;
       }
       
-      else if(mid == n-1)
+      else if(mid == n-1) // for last index,  we have to compare it only with its left element 
       {
           if(arr[n-1]>arr[n - 2])
           return n-1 ;
