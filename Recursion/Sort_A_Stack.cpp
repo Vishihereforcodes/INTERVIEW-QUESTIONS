ab45ba369fc4db65be8144f14f50fdfac067231b
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-06 00:17:49 +0530

    (Sort_A_Stack)  Approach Added

--- Recursion/Sort_A_Stack.cpp
+++ Recursion/Sort_A_Stack.cpp
@@ -1,3 +1,14 @@
+// Question:- Sort the given stack using recursion.
+
+/*
+Approach:-
+1. Base case:- If stack contains single element then it is already sorted.
+2. Otherwise, Pop the last element of stack and store it in temp variable now call sort fxn in rest of the stack(which will sort the rest stack).
+3. Now, only work is to push/insert the popped element in the vector back. So,in insert fxn, if vector is empty or the element is the greatest/ equaal of the elements
+in the stack then push the element .
+4. Otherwise, pop an element from stack and again check the condition through recursive call and atlast push the element back.
+*/
+
 void insert(stack<int>& s,int val)
 {
     if(s.empty() || s.top() <= val)
