58ab168dd79e78924681347bdc7b9b37ae0df248
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-06 00:40:49 +0530

    (Tower_Of_Hanio) Approach Added

--- Recursion/Tower_Of_Hanio.cpp
+++ Recursion/Tower_Of_Hanio.cpp
@@ -1,3 +1,18 @@
+/*
+Question:- We are given three towers one is source , one is helper other is destination . We have to shift all the disks from source to destnation using helper tower.
+There are some conditions which are:-
+1. We can't put larger disk on smaller one.
+2. We can take only one disk at a time.
+3. We can take help of helper tower to shift disks.
+*/
+
+/*
+Approach:-
+1. If there is only one disk then simply shift it at destination from source.
+2. Otherwise, Put all the disks at helper tower except the last one and move last disk to destonation.
+3. Atlast move remaining n-1 disks from helper to destination.
+*/
+
 int towerOfHanoi(int n,int source,int destination,int helper)
 {
     //If only one disk then move disk and return
