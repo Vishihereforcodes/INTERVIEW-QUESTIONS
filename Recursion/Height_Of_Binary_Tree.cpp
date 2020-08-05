52c96e15b089ef2a3e0cc867c1285ea6ce52688e
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-05 23:13:15 +0530

    added approach

--- Recursion/Height_Of_Binary_Tree.cpp
+++ Recursion/Height_Of_Binary_Tree.cpp
@@ -1,7 +1,14 @@
+// Question:- To find height of a given binary tree.
+
+/*Approach:- 
+1. Height of binary tree is the height of root node plus maximum of height of left subtree and right subtree.
+2. So, we will calculate height of left and right subtree through recursion and add the maximum one into height of root node(1).
+*/
+
 int heightOfBinaryTree(node* root)
 {
     if(root == NULL)
         return 0;
         
-   return 1 + max(heightOfBinaryTree(roo->left), heightOfBinaryTree(root->right));
+   return 1 + max(heightOfBinaryTree(root->left), heightOfBinaryTree(root->right));
 }
