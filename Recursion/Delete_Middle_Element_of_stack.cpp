92df4712dbc3e1f9df0730bdd74356d6f16d55e4
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-06 00:14:23 +0530

    (Delete_Middle_Element_of_stack) Approach Added

--- Recursion/Delete_Middle_Element_of_stack.cpp
+++ Recursion/Delete_Middle_Element_of_stack.cpp
@@ -1,3 +1,13 @@
+// Question:- Delete middle element of the given stack.
+
+/*
+Approach:-
+1. Base case:- If stack is empty then simply return it.
+2. Otherwise call solve fxn , in solve fxn , we will check if middle of the stack is at the top of the stack , if yes then pop the top .
+3. Otherwise, store the top of the stack in temp variable and call solve with k-1 until middle element is at top of the stack .
+4. Atlast push the temp again into the stack.
+*/
+
 stack<int> deleteMiddleElement(stack<int>s)
 {
     int n = s.size();
