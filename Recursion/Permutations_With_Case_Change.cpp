75a2815312d3c6d449e1a6ccc784d495a2b02eec
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-07 17:54:44 +0530

    Update Permutations_With_Case_Change.cpp

--- Recursion/Permutations_With_Case_Change.cpp
+++ Recursion/Permutations_With_Case_Change.cpp
@@ -1,6 +1,7 @@
-// Question:- You have to find all the permutations of the given lowercase string after changing lowecase to uppercase letters in the string.
-
 /*
+Question:- You have to find all the permutations of the given lowercase string after changing lowecase to uppercase letters in the string.
+Link :- (https://leetcode.com/problems/letter-case-permutation/)
+
 Approach:-
 1. Base case:- Whenever input string gets empty we get a permutation in output string.
 2. Now , we will have two outputs everytime based on our decisions (either add a character after converting it into uppercase or add it without converting into uppercase).
