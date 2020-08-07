856c2cc0188b7994eda465283fa48da9ee4245ee
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-07 16:32:08 +0530

    (Generate_all_balanced_parentheses) Approach Added

--- Recursion/Generate_all_balanced_parentheses.cpp
+++ Recursion/Generate_all_balanced_parentheses.cpp
@@ -1,3 +1,12 @@
+// Question:- You have to generate all the balanced parentheses with '(' and ')' .
+
+/*
+Approach:-
+1. Base case:- We will get a balanced parentheses everytime when count of opening and closing parentheses becomes zero.
+2. Otherwise, we can always push opening parentheses to our output until its zero.
+3. But if our opening parentheses count is less than the closing count then we can include it in our output to make it balanced.
+*/
+
 void solve(int open , int close , string op , vector<string>&v)
 {
 	if(open == 0 && close == 0) 
