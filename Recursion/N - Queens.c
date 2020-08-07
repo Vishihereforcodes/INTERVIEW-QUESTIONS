f06cb2ab7963f2959dcfe00c40c1a33594054d8f
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-07 20:06:00 +0530

    Update N - Queens.c

--- Recursion/N - Queens.c
+++ Recursion/N - Queens.c
@@ -35,7 +35,7 @@ int nQueens(int n,int board[][n],int queens)
             if(nQueens(n,board,queens-1))
                 return 1;//queen placed successfully
 
-            board[i][j] = 0;//remove queen(baktrack)
+            board[i][j] = 0;//remove queen(backtrack)
         }
     }
     return 0;
