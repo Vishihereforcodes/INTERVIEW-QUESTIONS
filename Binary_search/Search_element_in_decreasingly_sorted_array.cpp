53be9170271c207f065460646fce0aeb6b12e4bc
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-22 20:06:31 +0530

    Update Search_element_in_decreasingly_sorted_array.cpp

--- Binary_search/Search_element_in_decreasingly_sorted_array.cpp
+++ Binary_search/Search_element_in_decreasingly_sorted_array.cpp
@@ -1,35 +1,49 @@
 #include<iostream>
 using namespace std;
 
-int binarySearchInDecreasingSortedArray(int *arr , int n ,int num)
+int binarySearchInDecreasingSortedArray(int *arr , int n ,int target)
 {
 	int start = 0,end= n-1 ,mid;
 	
 	while(start<=end)
 	{
-		mid = start + (end-start)/2;
-		if(num == arr[mid])
+		mid = start + (end-start)/2; //to avoid stack overflow
+		
+		if(target == arr[mid]) //found our target
 		    return mid;
-		else if(num < arr[mid])
+		
+		//target is smaller, means it will be on right side
+		else if(target < arr[mid]) 
 		    start = mid + 1;
-		else
+		
+		//target is larger, means it will be on left side
+		else 
 		    end = mid -1;
 	}
 	return -1;
 }
-int main(){
-	
-	int n , num;
-	cout<<"Enter size of the array : "<<endl;
+
+int main()
+{
+	int n , target;
+	cout<<"Enter size of the array : ";
 	cin>>n;
 	int arr[n];
 	
 	for(int i=0;i<n;i++)
 	cin>>arr[i];
 	
-	cout<<"Enter the element to search : "<<endl;
-	cin>>num;
+	cout<<"Enter the target : ";
+	cin>>target;
 	
-	cout<<"Index of the searched element is = "<< binarySearchInDecreasingSortedArray(arr , n ,num);
+	int index = binarySearchInDecreasingSortedArray(arr , n ,num);
+	
+	//target is found
+	if(index != -1)
+		cout<<"Index of the searched element is = "<< index;
+	
+	//target is not found
+	else
+		cout << "Target is not found."
 	
 }
