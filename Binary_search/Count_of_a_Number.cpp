d8ffbd1e02161b61116961b53ebb28b3861e4f2b
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-22 21:03:56 +0530

    Update Count_of_a_Number.cpp
    FirstOccurance(arr , n ,num) - LastOccurance(arr , n ,num) will give error. if target is not they give answer as -2, instead of 0. all error is fixed now,

--- Binary_search/Count_of_a_Number.cpp
+++ Binary_search/Count_of_a_Number.cpp
@@ -1,7 +1,17 @@
+/*
+	Question : find total number of occurance of a element in a sorted array.
+	
+	How to do ?
+
+	Given array is sorted, so we can use binary search.Now in binary search , as we find a element we return that index.
+	but now we will store that index in temporary answer.and check in the left side for finding its first occurance and for last occurance we will check in right side.
+	The difference in the indexes will be our required answer.
+*/
+
 #include<iostream>
 using namespace std;
 
-
+T
 int FirstOccurance(int *arr , int n ,int num)
 {
 	int start = 0,end= n-1 ,mid;
@@ -10,6 +20,8 @@ int FirstOccurance(int *arr , int n ,int num)
 	while(start<=end)
 	{
 		mid = start + (end-start)/2;
+		
+		//if found then check in left side as well
 		if(num == arr[mid])
 		{
 			ans = mid;
@@ -32,6 +44,7 @@ int LastOccurance(int *arr , int n ,int num)
 	{
 		mid = start + (end-start)/2;
 		
+		//if found then check in right side as well
 		if(num == arr[mid])
 		{
 		    ans = mid ;
@@ -45,8 +58,8 @@ int LastOccurance(int *arr , int n ,int num)
 	return ans;
 }
 
-int main(){
-	
+int main()
+{
 	int n , num;
 	cout<<"Enter size of the array : "<<endl;
 	cin>>n;
@@ -58,6 +71,18 @@ int main(){
 	cout<<"Enter the element to search : "<<endl;
 	cin>>num;
 	
-	cout<<"Count of "<<num<<" is = "<< LastOccurance(arr , n ,num) - FirstOccurance(arr , n ,num) + 1;
+	int firstIndex = FirstOccurance(arr , n ,num);
+	
+	//target is found
+	if(firstIndex != -1)
+	{	
+		int secondIndex = LastOccurance(arr , n ,num);
+		
+		if(index != -1 )
+			cout << "Count of element is " << firstIndex - lastIndex + 1 << endl;
+	}
+	//target is not found
+	else
+		cout << "Count of element is 0.";
 	
 }
