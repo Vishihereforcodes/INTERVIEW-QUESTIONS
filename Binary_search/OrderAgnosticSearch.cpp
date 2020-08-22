268a1fa3dc5660b3d599ea56679bd0af65b684ee
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-22 20:24:49 +0530

    Update OrderAgnosticSearch.cpp

--- Binary_search/OrderAgnosticSearch.cpp
+++ Binary_search/OrderAgnosticSearch.cpp
@@ -1,9 +1,23 @@
-// Question 2 :- Given a sorted array but we dont know its ascending or descendingly sorted  . We have to search an element.
+/* 
+Question 2 :- Given a sorted array but we dont know its ascending or descendingly sorted  . We have to search an element.
 
+How to do ?
+simple divide this question in two part 
+1. find order of sorting(ascending/descending)
+2, search element using binary search
+
+step 1:
+1.If array size is 1. Then there will be no order....just we have to check that element is our target or not.
+2.we take first and last element and check the order(we can choose any pair).
+
+step 2:
+[binary search](https://github.com/urvashi-code1255/INTERVIEW-QUESTIONS/blob/master/Binary_search/Search_element_in_decreasingly_sorted_array.cpp)
+
+*/
 #include<iostream>
 using namespace std;
 
-int OrderAgnosticSearch(int *arr , int n ,int num)
+int OrderAgnosticSearch(int *arr , int n ,int target)
 {
 	int start = 0,end= n-1 ,mid;
 	
@@ -12,19 +26,20 @@ int OrderAgnosticSearch(int *arr , int n ,int num)
 	while(start<=end)
 	{
 		mid = start + (end-start)/2;
-		if(num == arr[mid])
+		
+		if(target == arr[mid])
 		    return mid;
 		
 		if(isAscending)
 		{
-			if(num > arr[mid])
+		    if(target > arr[mid])
 		        start = mid + 1;
 		    else
 		        end = mid -1;
 		}
 		else
 		{
-			if(num < arr[mid])
+		    if(target < arr[mid])
 		        start = mid + 1;
 		    else
 		        end = mid -1;
@@ -33,9 +48,10 @@ int OrderAgnosticSearch(int *arr , int n ,int num)
 	}
 	return -1;
 }
-int main(){
-	
-	int n , num;
+
+int main()
+{
+	int n ,target;
 	cout<<"Enter size of the array : "<<endl;
 	cin>>n;
 	int arr[n];
@@ -44,8 +60,12 @@ int main(){
 	cin>>arr[i];
 	
 	cout<<"Enter the element to search : "<<endl;
-	cin>>num;
+	cin>>target;
 	
-	cout<<"Index of the searched element is = "<< OrderAgnosticSearch(arr , n ,num);
+	int index = OrderAgnosticSearch(arr , n ,num);
+	if(index != -1)
+		cout<<"Index of the Target is = "<< index;
+	else
+		cout<< "Target is not found.";
 	
 }
