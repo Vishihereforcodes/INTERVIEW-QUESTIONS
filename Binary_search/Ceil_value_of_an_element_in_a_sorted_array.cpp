e378a40c1740f91f62361fdf480fb48716aa567e
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-23 14:34:11 +0530

    Update Ceil_value_of_an_element_in_a_sorted_array.cpp

--- Binary_search/Ceil_value_of_an_element_in_a_sorted_array.cpp
+++ Binary_search/Ceil_value_of_an_element_in_a_sorted_array.cpp
@@ -1,4 +1,12 @@
-// Ceil value of a number is the smallest number greater than that number in case of integers.
+/*
+  Ceil value of a number is the smallest number greater than that number in case of integers.
+
+  As Ceil value is greater than equal to that element. so will compare mid value with that element.
+  1. if values are equal,then will return mid value.
+  2. if mid value is larger than that element,then it may be the answer store it and find smaller number greater then that element.
+  3. if mid value is smaller than that element, then search in left subarray.
+  
+*/
 
 #include<iostream>
 using namespace std;
@@ -14,7 +22,8 @@ int CeilValue(int *arr , int n , int num)
 		if(arr[mid] == num) // if element is present in array then it is ceil value of itself.
 			return arr[mid] ;
 		
-		else if(arr[mid] > num){
+		else if(arr[mid] > num)
+		{
 			result = arr[mid];
 			end = mid - 1 ;
 		}
