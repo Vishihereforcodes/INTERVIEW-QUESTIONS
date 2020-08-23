3128551fb727310e11e640f1aab31fd36d9ff0c5
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-23 23:05:04 +0530

    Update K_Closest_Points_To_Origin.cpp

--- Heaps/K_Closest_Points_To_Origin.cpp
+++ Heaps/K_Closest_Points_To_Origin.cpp
@@ -1,3 +1,15 @@
+/* https://leetcode.com/problems/k-closest-points-to-origin/
+
+Question:- We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+(Here, the distance between two points on a plane is the Euclidean distance.)
+You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+
+Approach:- 
+As we have to find the k closest points so we will calculate the distance of point from origin using distance formula and for closest point , the distance should be minimum
+so we will make a maximum heap for it and we will take a pair in it and we will take the distance as its first and the points as its second.
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
