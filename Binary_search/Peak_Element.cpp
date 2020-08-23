753468648109947708a0e52124e3a21a6f56af35
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-23 18:14:10 +0530

    Update Peak_Element.cpp

--- Binary_search/Peak_Element.cpp
+++ Binary_search/Peak_Element.cpp
@@ -4,37 +4,39 @@
 int peakElement(int *arr, int n)
 {
   int start = 0 , end = n-1 , mid ;
+  
   while(start<=end)
   {
       mid = start + (end - start)/2;
+    
       if(mid>0 && mid<n-1) 
       {
         if(arr[mid] >= arr[mid-1] && arr[mid] >= arr[mid+1])
-        return mid ;
+          return mid ;
         
         else if(arr[mid-1] >= arr[mid]) 
-        end = mid - 1;
+          end = mid - 1;
         
         else
-        start = mid + 1;
+         start = mid + 1;
       }
       
       else if(mid == 0) // for index 0 we have to compare it only with the element in its right
       {
           if(arr[0] >= arr[1])
-          return 0 ;
+            return 0 ;
           
           else
-          return 1;
+            return 1;
       }
       
       else if(mid == n-1) // for last index,  we have to compare it only with its left element 
       {
           if(arr[n-1] >= arr[n - 2])
-          return n-1 ;
+           return n-1 ;
           
           else
-          return n-2 ;
+           return n-2 ;
       }
   }
 }
