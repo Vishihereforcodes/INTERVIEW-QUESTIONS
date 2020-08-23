d0918a801d206babd06e98b8686edda275597f6d
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-23 23:09:12 +0530

    Update Kth_Largest_Element.cpp

--- Heaps/Kth_Largest_Element.cpp
+++ Heaps/Kth_Largest_Element.cpp
@@ -1,3 +1,13 @@
+/* https://leetcode.com/problems/kth-largest-element-in-an-array/
+
+Question:- Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Approach:-
+As we have to find kth largest so we will make a minimum heap , and will pop the element from it if its size is greater than k because everytime its size is greater than k
+then it will have the element smaller than kth largest element at its top .
+
+*/
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
