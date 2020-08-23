470922b9f70929fa0272687d337ca368cbc1792c
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-23 22:57:58 +0530

    Update Find_K_Closest_Elements.cpp

--- Heaps/Find_K_Closest_Elements.cpp
+++ Heaps/Find_K_Closest_Elements.cpp
@@ -1,3 +1,14 @@
+/* https://leetcode.com/problems/find-k-closest-elements/ 
+
+Question:- Given a sorted array arr, two integers k and x , find the k closest elements to x in the array. 
+The result should also be sorted in ascending order. If there is a tie, the smaller elements are always preferred.
+
+Approach:- 
+We have to find closest elements which means that the distance between both elements should be minimum and as there is minimum word so we will make a maximum heap.
+we will take a pair in it in which first we will take the absolute diff of two elements and in seconn we will take the array element.
+
+*/
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
