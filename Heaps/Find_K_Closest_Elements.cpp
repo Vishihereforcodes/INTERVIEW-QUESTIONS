6228612baa469548b10cc702914a5b53da039446
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-24 15:18:14 +0530

    Update Find_K_Closest_Elements.cpp

--- Heaps/Find_K_Closest_Elements.cpp
+++ Heaps/Find_K_Closest_Elements.cpp
@@ -5,7 +5,7 @@ The result should also be sorted in ascending order. If there is a tie, the smal
 
 Approach:- 
 We have to find closest elements which means that the distance between both elements should be minimum and as there is minimum word so we will make a maximum heap.
-we will take a pair in it in which first we will take the absolute diff of two elements and in seconn we will take the array element.
+we will take a pair in it in which first we will take the absolute diff of two elements and in second we will take the array element.
 
 */
 
@@ -15,17 +15,20 @@ public:
         priority_queue<pair<int,int>>q;
         vector<int>ans;
         
-        for(int i = 0 ; i < arr.size() ; i++)
+        for(int i = 0 ,n = arr.size(); i < n ; i++)
         {
-            q.push(make_pair(abs(arr[i]-x) , arr[i]));
-            if(q.size() > k)
+            q.push({abs(arr[i]-x) , arr[i]});
+            
+            if(i+1 > k)
                 q.pop();
         }
+        
         while(!q.empty())
         {
             ans.push_back(q.top().second);
             q.pop();
         }
+        
         sort(ans.begin() , ans.end());
         return ans;
     }
