289f46f415b5216e80b59c6083a9139bfeac0dcc
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-24 21:34:58 +0530

    Update 01-Nearest_Greatest_to_Right.cpp

--- Stack/01-Nearest_Greatest_to_Right.cpp
+++ Stack/01-Nearest_Greatest_to_Right.cpp
@@ -7,22 +7,25 @@ void NextGreater(int *arr , int n)
     stack<int>s;
     for(int i = n - 1 ; i >= 0 ; i--)
     {
-    if(s.empty())
-        v.push_back(-1);
-    else if(!s.empty() && s.top() > arr[i])
-        v.push_back(s.top());
-    else if(!s.empty() && s.top() <= arr[i])
-    {
-        while(!s.empty() && s.top() <= arr[i])
-        {
-            s.pop();
-        }
         if(s.empty())
             v.push_back(-1);
-        else
-        v.push_back(s.top());
-    }
-    s.push(arr[i]);
+        
+        else if(!s.empty() && s.top() > arr[i])
+            v.push_back(s.top());
+        
+        else if(!s.empty() && s.top() <= arr[i])
+        {
+            while(!s.empty() && s.top() <= arr[i])
+            {
+                s.pop();
+            }
+            
+            if(s.empty())
+                v.push_back(-1);
+            else
+                v.push_back(s.top());
+        }
+        s.push(arr[i]);
     }
     reverse(v.begin() , v.end());
     
