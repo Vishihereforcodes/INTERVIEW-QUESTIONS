2b14c0ecf2938e41bc7631b17d8b7dd7633d3dc9
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-24 13:30:51 +0530

    Update Kth_Largest_Element.cpp
    usage of size() function will increase its time complexity to O

--- Heaps/Kth_Largest_Element.cpp
+++ Heaps/Kth_Largest_Element.cpp
@@ -8,16 +8,39 @@ then it will have the element smaller than kth largest element at its top .
 
 */
 
+//Solution 1: Time Complexity O(N*k*logk). extra k because of size() function
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
+    int findKthLargest(vector<int>& nums, int k) 
+    {
         priority_queue<int,vector<int>,greater<int>>q;
-        for(auto i : nums)
+        
+        for(auto i : nums) //O(N)
         {
-            q.push(i);
-            if(q.size() > k)
+            q.push(i);  //O(logk)
+            if(q.size() > k) //O(K)
                 q.pop();
         }
+        
+        return q.top() ;
+    }
+};
+
+//Solution 2: Same solution but without use of size() function because time complexity of size() is O(K)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        priority_queue<int,vector<int>,greater<int>>q; 
+        
+        for(int i=0, n = nums.size(); i<n ; i++) //O(N)
+        {
+            q.push(nums[i]);  //O(logk)
+            
+            if(i+1 > k)  //O(1)
+                q.pop();
+        }
+        
         return q.top() ;
     }
 };
