4d38de66fb0b2d3e34fce22d42fa85c8aced6f5a
Ritika <Ankita@URVASHIANAND.ushi>
2020-08-24 21:27:37 +0530

    stack added

---
+++ Stack/01-Nearest_Greatest_to_Right.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void NextGreater(int *arr , int n)
+{
+    vector<int>v;
+    stack<int>s;
+    for(int i = n - 1 ; i >= 0 ; i--)
+    {
+    if(s.empty())
+        v.push_back(-1);
+    else if(!s.empty() && s.top() > arr[i])
+        v.push_back(s.top());
+    else if(!s.empty() && s.top() <= arr[i])
+    {
+        while(!s.empty() && s.top() <= arr[i])
+        {
+            s.pop();
+        }
+        if(s.empty())
+            v.push_back(-1);
+        else
+        v.push_back(s.top());
+    }
+    s.push(arr[i]);
+    }
+    reverse(v.begin() , v.end());
+    
+    for(auto i : v)
+    cout<<i<<" ";
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+    cin>>arr[i];
+
+    NextGreater(arr , n);
+
+}
