623d22419abb176216ebbc2c4aca65145c557502
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-25 14:27:02 +0530

    Update 03-Nearest_Smaller_To_Left.cpp

--- Stack/03-Nearest_Smaller_To_Left.cpp
+++ Stack/03-Nearest_Smaller_To_Left.cpp
@@ -13,8 +13,8 @@
     
     So will store element from left in a stack and check three conditions.
     1. if stack is empty that means next greater element is not posssible.So push -1.
-    2. if stack.top() is greater than i'th element that is the answer for that element.So push stack top element.
-    3. if stack.top() is smaller than i'th element then pop until above two condition is met.
+    2. if stack.top() is smaller than i'th element that is the answer for that element.So push stack top element.
+    3. if stack.top() is greater than i'th element then pop until above two condition is met.
 
 */
 #include<bits/stdc++.h>
