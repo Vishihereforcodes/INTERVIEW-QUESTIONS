9faf9e72428b03c97cbec8cb0cd93cd85c7e799c
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-25 16:33:23 +0530

    Update 08-Rain_Water_Trapping.cpp

--- Stack/08-Rain_Water_Trapping.cpp
+++ Stack/08-Rain_Water_Trapping.cpp
@@ -1,13 +1,23 @@
 /* https://leetcode.com/problems/trapping-rain-water/
-
+	
+	Hpw tp do ?
+	
+	1.Calculate max height of building from left.
+	1.Calculate max height of building from right.
+	2.Calculate area for each index. Area will be min(maxHeightLeft[i] , maxHeightRight[i]) - height[i].
+	4.Sum up the area.
+	
 */
 #include<bits/stdc++.h>
 using namespace std;
 
 int MaximumRainWaterTrapped(int *height , int n)
 {
+    if(n == 0)
+    	return 0;
+	
     int maxHeightLeft[n] , maxHeightRight[n] , water[n];
-
+	
     maxHeightLeft[0] = height[0];
     for(int i = 1 ; i < n ; i++)
     {
@@ -19,17 +29,14 @@ int MaximumRainWaterTrapped(int *height , int n)
     {
         maxHeightRight[i] = max(maxHeightRight[i+1] , height[i]);
     }
-
+    
+    int sum = 0 ;
     for(int i = 0 ; i < n ; i++)
     {
         water[i] = min(maxHeightLeft[i] , maxHeightRight[i]) - height[i];
-    }
-
-    int sum = 0 ; 
-    for(int  i = 0 ; i < n ; i++)
-    {
         sum += water[i];
     }
+
     return sum;
 }
 
