ebb5f5076daab29030c44e14aa998ff42be0c585
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-30 05:27:57 +0530

    Update 08_Knapsack_with_Duplicate_items.cpp

--- DP/08_Knapsack_with_Duplicate_items.cpp
+++ DP/08_Knapsack_with_Duplicate_items.cpp
@@ -4,6 +4,11 @@ Question:- We are given a knapsack and we have an array of items with price of e
 including the items in the knapsack (ONE ITEM CAN BE ADDED MORE THAN ONCE).
 
 Approach:-
+1. As in question , we have to calculate the maximum profit(Optimum) , So we can apply dp here.
+2. Initialisation:- Either in case of empty array of items or if capacity of knapsack is zero , there will be no profit or zero profit so we will initialise first row and 
+first column by 0.
+3. Then we will only make choice from the items whose weight is less than or equal to the weight of knapsack and everytime we will reduce our knapsack capacity after 
+including the item but we will not remove that particular element from our further choice.
 
 
 */
