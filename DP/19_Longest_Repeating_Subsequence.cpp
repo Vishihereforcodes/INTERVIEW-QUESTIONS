788f9aee498a62d70dc4367eeeda374af0497547
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-31 05:12:19 +0530

    Update 19_Longest_Repeating_Subsequence.cpp

--- DP/19_Longest_Repeating_Subsequence.cpp
+++ DP/19_Longest_Repeating_Subsequence.cpp
@@ -1,4 +1,11 @@
-// https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence/0
+/* (https://practice.geeksforgeeks.org/problems/longest-repeating-subsequence/0)
+
+Question:- Given a string str, find length of the longest repeating subseequence such that the two subsequence don’t have same string character at same position,
+i.e., any i’th character in the two subsequences shouldn’t have the same index in the original string.
+
+Approach:-
+
+*/
 
 #include <iostream>
 #include <string>
