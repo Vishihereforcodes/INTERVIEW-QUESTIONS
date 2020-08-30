970f52571689973ac3bbd00feb2bd12a1df5e3b3
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-31 05:01:59 +0530

    Update 18_Printing_Shortest_Common_Supersequence.cpp

--- DP/18_Printing_Shortest_Common_Supersequence.cpp
+++ DP/18_Printing_Shortest_Common_Supersequence.cpp
@@ -1,5 +1,9 @@
 /* (https://leetcode.com/problems/shortest-common-supersequence/)
 
+Question:- Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences.  If multiple answers exist, you may return any of them.
+
+Approach:-
+
 */
 
 class Solution
