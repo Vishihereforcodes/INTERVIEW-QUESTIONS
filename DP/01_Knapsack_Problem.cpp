33965c4acfd51e71ce44f64df8561e52d9a7074d
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-30 12:46:26 +0530

    Update 01_Knapsack_Problem.cpp

--- DP/01_Knapsack_Problem.cpp
+++ DP/01_Knapsack_Problem.cpp
@@ -15,6 +15,36 @@ including the item .
 #include <iostream>
 using namespace std;
 
+//Solution 1 : Using Recursion
+int knapsack(int value[], int weight[], int capacity, int N)
+{
+    if (N == 0 || capacity == 0)
+        return 0;
+
+    if (weight[N - 1] <= capacity)
+        return max(value[N - 1] + knapsack(value, weight, capacity - weight[N - 1], N - 1), knapsack(value, weight, capacity, N - 1));
+    else
+        return knapsack(value, weight, capacity, N - 1);
+}
+
+//Solution 2 : Using Recursion + Memoization
+int V[101][1001];
+
+int knapsack(int value[], int weight[], int capacity, int N)
+{
+    if (N == 0 || capacity == 0)
+        return 0;
+
+    if (V[N][capacity] != -1)
+        return V[N][capacity];
+
+    if (weight[N - 1] <= capacity)
+        return V[N][capacity] = max(value[N - 1] + knapsack(value, weight, capacity - weight[N - 1], N - 1), knapsack(value, weight, capacity, N - 1));
+    else
+        return V[N][capacity] = knapsack(value, weight, capacity, N - 1);
+}
+
+//Solution 3 : DP
 int knapsack(int value[], int weight[], int capacity, int N)
 {
     int DP[N + 1][capacity + 1];
@@ -37,8 +67,13 @@ int knapsack(int value[], int weight[], int capacity, int N)
 
 int main()
 {
-    int T, N, capacity;
+    int T, N, capacity, ans;
     cin >> T;
+
+    //For Solution 2
+    for (int i = 0; i < 101; i++)
+        for (int j = 0; j < 1001; j++)
+            V[i][j] = -1;
 
     while (T--)
     {
@@ -51,7 +86,10 @@ int main()
         for (int i = 0; i < N; i++)
             cin >> weight[i];
 
-        cout << knapsack(value, weight, capacity, N) << "\n";
+        ans = knapsack(value, weight, capacity, N);
+
+        cout << ans << "\n";
     }
 
     return 0;
+}
