c9e4ea74a1eafbfa1f0e577e4263560c0fba3d01
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-08-31 07:06:15 +0530

    Update 20_Matrix_Chain_Multiplication.cpp

--- DP/20_Matrix_Chain_Multiplication.cpp
+++ DP/20_Matrix_Chain_Multiplication.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
+#include<stdio.h>
 using namespace std;
+int DP[1001][1001];
 
+// Solution1:- Recursive..
 int Cost(int *arr , int i , int j)
 {
     if(i >= j)
@@ -18,6 +21,28 @@ int Cost(int *arr , int i , int j)
     return Minimum;
     
 }
+
+// Solution2:- Recursive + Memoization..
+int Cost(int *arr , int i , int j)
+{
+    if(i >= j)
+        return 0;
+    
+    int Minimum = INT_MAX;
+
+    if(DP[i][j] != -1)
+        return DP[i][j];
+        
+    for(int k = i ; k <= j-1 ; k++)
+    {
+        int tempAns = Cost(arr , i , k) + Cost(arr , k+1 , j) + arr[i-1]*arr[k]*arr[j];
+
+        if(tempAns < Minimum)
+            Minimum = tempAns;
+    }
+    return DP[i][j]=Minimum;
+    
+}
 int main()
 {
     int n;
@@ -28,6 +53,8 @@ int main()
     {
         cin>>arr[i];
     }
+    memset(DP , -1 , sizeof(DP));
+    
     int i = 1 , j = n-1 ;
     cout<<"Minimum cost of matrix chain multiplication is "<<Cost(arr , i , j);
 }
