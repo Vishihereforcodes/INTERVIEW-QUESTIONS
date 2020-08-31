7db8d52a2cca2595887b4243df605bb695cf0c3d
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-08-31 14:47:34 +0530

    Update 14_Shortest_Common_Supersequence.cpp

--- DP/14_Shortest_Common_Supersequence.cpp
+++ DP/14_Shortest_Common_Supersequence.cpp
@@ -3,12 +3,12 @@
 Question:- Given two strings str1 and str2, find the shortest string that has both str1 and str2 as subsequences.
 
 Approach:- 
-As we know , to get shortest common supersequence we have to include a particular letter only once if it is present in both strings so to include the common letter once we have to
-subtract the longest common supersequence from the total length of both strings .
+As we know , to get shortest common supersequence we have to include a particular letter only once if it is present in both strings so to include the common letter once,
+we have to subtract the longest common subsequence from the total length of both strings .
 
 1. initialisation:- when any of the two strings is empty then empty subsequence will be common whose size will be zero.
-2. Code:- If letters at particular index of the matrix is equal then will include that length of longest subsequence will be one greater than the length at one minus that index
-else will not include that length and length will be the maximum of one less in the index.
+2. Code:- If letters at particular index of the matrix is equal then will include that length of longest subsequence will be one greater than the length at one minus that
+index else will not include that length and length will be the maximum of one less in the index.
 
 using these steps we will get length of longest common subsequence and substracting it from total length of both strings we will get our shortest common supersequence length.
 
