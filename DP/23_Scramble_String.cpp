5dd27533a264e3a7f5291727bcabee9aa6a102e2
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-01 15:16:14 +0530

    Update 23_Scramble_String.cpp

--- DP/23_Scramble_String.cpp
+++ DP/23_Scramble_String.cpp
@@ -11,20 +11,30 @@ class Solution
 public:
     bool isScramble(string s1, string s2)
     {
+        // Equal strings are scrambled.
         if (s1.compare(s2) == 0)
             return true;
-
+        
+        // Empty string is not scrambled.
         if (s1 == "")
             return false;
 
         int n = s1.length();
-        bool flag = false;
-
+        bool flag = false; 
+        
+        // Traversing both strings we will check following conditions..
         for (int i = 1; i < n; i++)
         {
+            // if at any node.
+            // if first half of the first string is scramble of second half of second string and 
+            // second half of the first string is scramble of first half of second string 
             if ((isScramble(s1.substr(0, i), s2.substr(n - i, i)) && isScramble(s1.substr(i, n - i), s2.substr(0, n - i))) 
+            // OR
+            // if first half of the first string is scramble of first half of second string and 
+            // second half of the first string is scramble of second half of second string 
             ||  (isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i, n - i), s2.substr(i, n - i))))
             {
+                // if any of these conditions are true then the two strings are scrambled , we will return true .
                 flag = true;
                 break;
             }
@@ -46,9 +56,11 @@ public:
         if (s1 == "")
             return false;
 
+        // We will generate a unique key using the two strings as the key of the map as we always take those variables as key which are changing .
         string key = s1;
         key += " " + s2;
 
+        // if key value is already present then no need to recalculate it.
         if (m.find(key) != m.end())
             return m[key];
 
@@ -64,6 +76,7 @@ public:
                 break;
             }
         }
+        // Otherwise we will store the value of the particluar key in the map so that it can be used later.
         return m[key] = flag;
     }
 };
