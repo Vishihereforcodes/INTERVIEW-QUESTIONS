999a4b21175928384c6b98b1fdbaa9a6f2671f2d
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-01 14:58:23 +0530

    Update 21_Palindromic_Partitioning.cpp

--- DP/21_Palindromic_Partitioning.cpp
+++ DP/21_Palindromic_Partitioning.cpp
@@ -28,15 +28,18 @@ int Partitions(string s, int i, int j)
     // if string is empty or of only one element then its already a palindrome.
     if (i >= j)
         return 0;
-
+   
+    // if string is already palindrome then no cuts are required.
     if (isPalindrome(s, i, j))
         return 0;
 
     int minimum = INT_MAX;
+    // we will divide the string into two parts and will find the no of cuts for the both parts and will add 1 to it as we divided the string into two parts it is also a cut.
     for (int k = i; k <= j - 1; k++)
     {
         int tempAns = 1 + Partitions(s, i, k) + Partitions(s, k + 1, j);
 
+       // will get the minimum cuts.
         if (tempAns < minimum)
             minimum = tempAns;
     }
@@ -53,6 +56,7 @@ int Partitions(string s, int i, int j)
     if (isPalindrome(s, i, j))
         return 0;
 
+    // will check if we have already processed that particular part answer.
     if (DP[i][j] != -1)
         return DP[i][j];
 
@@ -64,6 +68,7 @@ int Partitions(string s, int i, int j)
         if (tempAns < minimum)
             minimum = tempAns;
     }
+    // storing the answer in matrix so that it can be used later.
     return DP[i][j] = minimum;
 }
 
@@ -76,7 +81,8 @@ int Partitions(string s, int i, int j)
 
     if (isPalindrome(s, i, j))
         return 0;
-
+   
+    // will check if we have already processed that particular part answer.
     if (DP[i][j] != -1)
         return DP[i][j];
 
@@ -85,16 +91,18 @@ int Partitions(string s, int i, int j)
     int minimum = INT_MAX;
     for (int k = i; k <= j - 1; k++)
     {
+        // will check if we already have the answer for the left part in our matrix.
         if (DP[i][k] != -1)
             left = DP[i][k];
         else
-            left = Partitions(s, i, k);
+            left = Partitions(s, i, k); // otherwise find the ans and store in matrix
         DP[i][k] = left;
-
+        
+        // will check if we already have the answer for the right part in our matrix.
         if (DP[k + 1][j] != -1)
             right = DP[k + 1][j];
         else
-            right = Partitions(s, k + 1, j);
+            right = Partitions(s, k + 1, j); // otherwise find the ans and store in matrix
         DP[k + 1][j] = right;
 
         int tempAns = 1 + left + right;
