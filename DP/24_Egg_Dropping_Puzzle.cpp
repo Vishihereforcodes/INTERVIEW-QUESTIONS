d7542d87e034831926962bb01a5e2c501c6e45ef
Vishal <59874304+Vishal-raj-1@users.noreply.github.com>
2020-09-01 15:24:50 +0530

    Update 24_Egg_Dropping_Puzzle.cpp

--- DP/24_Egg_Dropping_Puzzle.cpp
+++ DP/24_Egg_Dropping_Puzzle.cpp
@@ -26,6 +26,26 @@ int eggDropPuzzle(int eggs, int floors)
     return minimum;
 }
 
+// Solution 2: Using Recursion + Memoization
+int DP[11][51];
+int eggDropPuzzle(int eggs, int floors)
+{
+    if(floors == 1 || eggs == 1)
+        return floors;
+
+    if(DP[eggs][floors] != -1)
+        return DP[eggs][floors];
+
+    int minimum = INT_MAX;
+    for (int k = 1; k <= floors; k++)
+    {
+        int tempAns = 1 + max(eggDropPuzzle(eggs-1, k-1), eggDropPuzzle(eggs, floors-k));
+
+        minimum = min(tempAns, minimum);
+    }
+    return DP[eggs][floors] = minimum;
+}
+
 int main()
 {
     int t, eggs, floors;
@@ -34,7 +54,7 @@ int main()
     while (t--)
     {
         cin >> eggs >> floors;
-
+        memset(DP, -1, sizeof(DP)); //for solution 2
         cout << eggDropPuzzle(eggs, floors) << "\n";
     }
 }
