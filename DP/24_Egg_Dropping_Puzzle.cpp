34455f109181e35323d89cec66a8f40237cd2d31
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-09-01 15:30:10 +0530

    Update 24_Egg_Dropping_Puzzle.cpp

--- DP/24_Egg_Dropping_Puzzle.cpp
+++ DP/24_Egg_Dropping_Puzzle.cpp
@@ -1,6 +1,6 @@
 /* (https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle/0)
 
-Question:- We are given no of eggs and no of floors . We have to find the minimum no of ways to find the threshold floor .
+Question:- We are given no of eggs and no of floors . We have to find the minimum no of attempts to find the threshold floor .
 Threshold floor is the first possible floor from which if we will throw the egg , it will not break.
 
 */
@@ -11,12 +11,15 @@ using namespace std;
 // Solution 1: Using Recursion
 int eggDropPuzzle(int eggs, int floors)
 {
-    // If there is only one floor then at worst case it will be the only threshold floor if egg will not break throwing from it.
-    // If there is only one egg then in worst case
+    // If there is only one floor then we will get our threshold floor only after throwing one egg , as after throwing one egg from it the egg will either break or saved.
+    // If there is only one egg then we will start throwing the egg from lowermost building so that it will not break before reaching the threshold floor , 
+    // so in worst case, egg will break on lowermost foor and it will be threshold floor.
     if(floors == 1 || eggs == 1)
         return 1;
 
     int minimum = INT_MAX;
+    // We will break our floors into two parts and will check in both parts by throwing the eggs and will get attemps from both and 
+    // will add one to maximum of both attemps for the middle one.
     for (int k = 1; k <= floors; k++)
     {
         int tempAns = 1 + max(eggDropPuzzle(eggs-1, k-1), eggDropPuzzle(eggs, floors-k));
@@ -24,26 +27,6 @@ int eggDropPuzzle(int eggs, int floors)
         minimum = min(tempAns, minimum);
     }
     return minimum;
-}
-
-// Solution 2: Using Recursion + Memoization
-int DP[11][51];
-int eggDropPuzzle(int eggs, int floors)
-{
-    if(floors == 1 || eggs == 1)
-        return floors;
-
-    if(DP[eggs][floors] != -1)
-        return DP[eggs][floors];
-
-    int minimum = INT_MAX;
-    for (int k = 1; k <= floors; k++)
-    {
-        int tempAns = 1 + max(eggDropPuzzle(eggs-1, k-1), eggDropPuzzle(eggs, floors-k));
-
-        minimum = min(tempAns, minimum);
-    }
-    return DP[eggs][floors] = minimum;
 }
 
 int main()
@@ -54,7 +37,7 @@ int main()
     while (t--)
     {
         cin >> eggs >> floors;
-        memset(DP, -1, sizeof(DP)); //for solution 2
+
         cout << eggDropPuzzle(eggs, floors) << "\n";
     }
 }
